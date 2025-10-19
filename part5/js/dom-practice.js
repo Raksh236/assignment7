183c2fb64b65e2c099fdfe3fd034598a0f33e65a
Rakshitha Arun Kumar <rakshitha.two3@gmail.com>
2025-10-18 23:15:01 -0700

    Initial commit for assignment7

--- part5/js/dom-practice.js
+++ part5/js/dom-practice.js
@@ -1,2 +1,26 @@
-function oldSchoolClick(){alert("I have been clicked")}
-window.addEventListener("DOMContentLoaded",()=>{const btn2=document.getElementById("btn2");btn2.onclick=function(){alert("I have been clicked")};function step3Handler(){alert("I have been clicked")}const btn3=document.getElementById("btn3");btn3.addEventListener("click",step3Handler);btn3.addEventListener("click",function(){alert("I have been clicked (anonymous callback)")})});
+function oldSchoolClick() {
+  alert("I have been clicked");
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const btn2 = document.getElementById("btn2");
+  btn2.onclick = function () {
+    alert("I have been clicked");
+  };
+
+  function step3Handler() {
+    alert("I have been clicked");
+  }
+  const btn3 = document.getElementById("btn3");
+  btn3.addEventListener("click", step3Handler);
+
+  const btn4 = document.getElementById("btn4");
+  btn4.addEventListener("click", function () {
+    alert("I have been clicked");
+  });
+
+  const btn5 = document.getElementById("btn5");
+  btn5.addEventListener("click", function () {
+    alert("I have been clicked (script in head using defer)");
+  });
+});
